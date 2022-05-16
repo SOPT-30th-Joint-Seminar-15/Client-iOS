ca04835bcc6e18ac5fcfbdb6376c45dd092607bd
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-17 01:44:08 +0900

    [Style] Custom NavigationBar UI 구성 (#5)

---
+++ 29cm-iOS/29cm-iOS/Source/Presentation/Common/NavigationBarView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBarView.swift
+//  29cm-iOS
+//
+//  Created by 정은희 on 2022/05/16.
+//
+
+import UIKit
+
+class NavigationBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
