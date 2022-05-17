a2355487ba241fa5007fdca23e30d37ad03f8a38
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-17 21:07:04 +0900

    [Style] GeneralCheckCollectionViewCell UI 구성 (#9)

---
+++ 29cm-iOS/29cm-iOS/Source/Presentation/Create/Views/Cells/Hee/GeneralCheckCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralCheckCollectionViewCell.swift
+//  29cm-iOS
+//
+//  Created by 정은희 on 2022/05/17.
+//
+
+import UIKit
+
+class GeneralCheckCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
