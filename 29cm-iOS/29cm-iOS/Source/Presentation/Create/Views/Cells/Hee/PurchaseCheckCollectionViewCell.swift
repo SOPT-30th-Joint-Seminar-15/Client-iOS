6681acc68fdb7144487277f7c82fb25a8195f9c9
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-17 21:01:18 +0900

    [Style] PurchaseCheckCollectionViewCell UI 구성 (#9)

---
+++ 29cm-iOS/29cm-iOS/Source/Presentation/Create/Views/Cells/Hee/PurchaseCheckCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PurchaseCheckCollectionViewCell.swift
+//  29cm-iOS
+//
+//  Created by 정은희 on 2022/05/17.
+//
+
+import UIKit
+
+final class PurchaseCheckCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
