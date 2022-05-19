23390914329221b9ab0877e6f97dcea2a919b746
Jimin Kim <0305jimin@gmail.com>
2022-05-19 15:15:09 +0900

    [Style] PostListTableViewCell UI(#11)

--- 29cm-iOS/29cm-iOS/Source/Presentation/Read/Controllers/PostListTableViewCell.swift
+++ 29cm-iOS/29cm-iOS/Source/Presentation/Read/Controllers/PostListTableViewCell.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class PostListTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var deleteButton: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+       
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
