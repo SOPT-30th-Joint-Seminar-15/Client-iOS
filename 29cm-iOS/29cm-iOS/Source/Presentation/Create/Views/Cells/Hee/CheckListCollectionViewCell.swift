0a3af1ae35a2e0f09578de825e9e0877a1259cc4
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-19 15:18:29 +0900

    [Edit] UI Design Structure 변경 (#9)
    - 개별 컬렉션 뷰 셀 (6개) → 단일 컬렉션 뷰 셀 (2개)

---
+++ 29cm-iOS/29cm-iOS/Source/Presentation/Create/Views/Cells/Hee/CheckListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CheckListCollectionViewCell.swift
+//  29cm-iOS
+//
+//  Created by 정은희 on 2022/05/19.
+//
+
+import UIKit
+
+final class CheckListCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
