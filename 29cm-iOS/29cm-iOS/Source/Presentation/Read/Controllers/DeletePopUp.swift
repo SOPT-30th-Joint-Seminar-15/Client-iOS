59d27f5f6e6c8f5e986cbb5d6d865485887e5655
Jimin Kim <0305jimin@gmail.com>
2022-05-25 12:22:42 +0900

    [Style] DeletePopUp UI(#15)

---
+++ 29cm-iOS/29cm-iOS/Source/Presentation/Read/Controllers/DeletePopUp.swift
@@ -0,0 +1,29 @@
+//
+//  DeletePopUp.swift
+//  29cm-iOS
+//
+//  Created by 김지민 on 2022/05/25.
+//
+
+import UIKit
+
+class DeletePopUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
