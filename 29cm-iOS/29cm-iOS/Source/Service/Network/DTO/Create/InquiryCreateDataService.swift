41a448f42aca2193f21e5b6d1ed0a8200cd4f1ca
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-05-30 20:20:18 +0900

    [Set] 서버통신 초기 세팅 (#17)
    - 예시 → 문의 생성 POST 요청 관련 모델 및 서비스 코드 생성

---
+++ 29cm-iOS/29cm-iOS/Source/Service/Network/DTO/Create/InquiryCreateDataService.swift
@@ -0,0 +1,87 @@
+//
+//  InquiryCreateDataService.swift
+//  29cm-iOS
+//
+//  Created by 정은희 on 2022/05/30.
+//
+
+import Foundation
+
+import Alamofire
+
+struct InquiryCreateDataService {
+    static let shared = InquiryCreateDataService()
+
+    func inquiryCreate(userId: String,
+                       email: String,
+                       inquiryCategory: String,
+                       orderNum: String,
+                       title: String,
+                       content: String,
+                       isSubscribed: Bool,
+                       completion: @escaping (NetworkResult<Any>) -> Void) {
+        
+        let url = Const.APIConstants.inquiryCreate
+        let header: HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        let body: Parameters = [
+            "userID": userId,
+            "email": email,
+            "inquiryCategory": inquiryCategory,
+            "orderNum": orderNum,
+            "title": title,
+            "content": content,
+            "isSubscribed": isSubscribed
+        ]
+        
+        let dataRequest = AF.request(url,
+                                     method: .post,
+                                     parameters: body,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode,
+                      let value = response.value
+                else { return }
+                print("응답 상태!!!!", statusCode)
+                let networkResult = self.judgeStatus(by: statusCode, in: value)
+                dump(networkResult)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+}
+
+extension InquiryCreateDataService {
+    private func judgeStatus(by statusCode: Int, in data: Data) -> NetworkResult<Any> {
+        switch statusCode {
+        case 200:
+            return isValidData(in: data)
+        case 400:
+            return isUsedPathErr(in: data)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func isValidData(in data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(InquiryCreateData.self, from: data)
+        else { return .pathErr }
+        return .success(decodedData)
+    }
+    
+    private func isUsedPathErr(in data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(InquiryCreateData.self, from: data)
+        else { return .pathErr }
+        return .requestErr(decodedData)
+    }
+}
